9314864cd0ebb12eab328e291a4a4dcafe2846c8
unknown <>
2016-07-05 18:56:55 +0800

    导入原始文件

---
+++ rap4g-common/src/main/java/com/zufengze/rap4g/config/PRIVATE_CONFIG.java
@@ -0,0 +1,7 @@
+package com.zufengze.rap4g.config;
+
+public class PRIVATE_CONFIG {
+    public static final String mailUserName = "rap@domain.com";
+    public static final String mailPassword = "";
+    public static final String adminPassword = "";
+}
